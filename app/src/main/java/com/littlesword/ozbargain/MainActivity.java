c943194c11ba3c98730c597bfed01bda09d42cf5
Wenchao Kong <wenchao.kong@gmail.com>
2015-11-18 16:50:50 +1100

    testing periodic task for retrieving bargains in background

--- app/src/main/java/com/littlesword/ozbargain/MainActivity.java
+++ app/src/main/java/com/littlesword/ozbargain/MainActivity.java
@@ -1,5 +1,9 @@
 package com.littlesword.ozbargain;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,9 +14,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.facebook.common.logging.FLog;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.littlesword.ozbargain.network.APIImp;
+import com.littlesword.ozbargain.scheduler.BargainReceiver;
 import com.littlesword.ozbargain.util.DocExtractor;
 import com.littlesword.ozbargain.view.CategoryFragment;
 
@@ -21,6 +25,7 @@ import org.jsoup.nodes.Document;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.GregorianCalendar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -61,9 +66,30 @@ public class MainActivity extends AppCompatActivity
         mNavigationView.getMenu().getItem(0).setCheckable(true);
         mNavigationView.getMenu().getItem(0).setChecked(true);
         onNavigationItemSelected(mNavigationView.getMenu().getItem(0));
+        scheduleAlarm();
     }
 
+    public void scheduleAlarm()
+    {
+        // time at which alarm will be scheduled here alarm is scheduled at 1 day from current time,
+        // we fetch  the current time in milliseconds and added 1 day time
+        // i.e. 24*60*60*1000= 86,400,000   milliseconds in a day
+        Long time = new GregorianCalendar().getTimeInMillis()+3000;
 
+        // create an Intent and set the class which will execute when Alarm triggers, here we have
+        // given AlarmReciever in the Intent, the onRecieve() method of this class will execute when
+        // alarm triggers and
+        //we will write the code to send SMS inside onRecieve() method pf Alarmreciever class
+        Intent intentAlarm = new Intent(this, BargainReceiver.class);
+
+        // create the object
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        //set the alarm for particular time
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, time,
+                3000, PendingIntent.getBroadcast(this, 1, intentAlarm, PendingIntent.FLAG_CANCEL_CURRENT));
+
+    }
     public String readTextFile() {
         InputStream inputStream = getResources().openRawResource(R.raw.sad); // getting XML
 
---
+++ app/src/main/java/com/littlesword/ozbargain/scheduler/BargainReceiver.java
@@ -0,0 +1,16 @@
+package com.littlesword.ozbargain.scheduler;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by kongw1 on 18/11/15.
+ */
+public class BargainReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("wenchaop", "hahahahaha");
+    }
+}
