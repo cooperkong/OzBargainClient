98503581653bafb146157a2a979fae0b391930c1
Wenchao Kong <wenchao.kong@gmail.com>
2015-11-19 11:23:19 +1100

    url constants

---
+++ app/src/main/java/com/littlesword/ozbargain/util/CatUrls.java
@@ -0,0 +1,17 @@
+package com.littlesword.ozbargain.util;
+
+/**
+ * Created by kongw1 on 19/11/15.
+ */
+public interface CatUrls {
+    String BASE_URL = "https://www.ozbargain.com.au";
+
+    String ELECTRONICS = "/cat/electrical-electronics";
+    String FOOD = "/cat/food-grocery";
+    String GAMING = "/cat/gaming";
+    String FASHION = "/cat/fashion-apparel";
+    String HOME = "/cat/home-garden";
+    String COMPUTING = "/cat/computing";
+    String TRAVEL = "/cat/travel";
+    String ENTERTAINMENT = "/cat/entertainment";
+}
