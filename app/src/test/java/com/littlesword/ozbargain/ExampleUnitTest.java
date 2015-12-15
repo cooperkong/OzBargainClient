5c8ed1ea048063b4d1d27088fd05b61240f5a1ab
Wenchao Kong <wenchao.kong@gmail.com>
2015-12-15 14:15:14 +1100

    restyle bargain details fragment

--- app/src/test/java/com/littlesword/ozbargain/ExampleUnitTest.java
+++ app/src/test/java/com/littlesword/ozbargain/ExampleUnitTest.java
@@ -5,6 +5,7 @@ import com.littlesword.ozbargain.util.TimeUtil;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static com.google.common.truth.Truth.assertThat;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -17,13 +18,13 @@ public class ExampleUnitTest {
 
     @Test
     public void testTimeUtil(){
-        assertEquals(true, TimeUtil.isNew("11/12/2015 14:29","11/12/2015 14:34" ));
-        assertEquals(true, TimeUtil.isNew("11/12/2016 14:29","11/12/2017 14:34" ));
-        assertEquals(true, TimeUtil.isNew("11/10/2015 06:29","02/11/2015 14:34" ));
+        assertThat(true).isEqualTo(TimeUtil.isNew("11/12/2015 14:29","11/12/2015 14:34" ));
+        assertThat(true).isEqualTo(TimeUtil.isNew("11/12/2016 14:29","11/12/2017 14:34" ));
+        assertThat(true).isEqualTo(TimeUtil.isNew("11/10/2015 06:29","02/11/2015 14:34" ));
+        assertThat(true).isEqualTo(TimeUtil.isNew("11/10/2015 06:29","02/11/2015 14:34" ));
 
-
-        assertEquals(false, TimeUtil.isNew("11/10/2015 06:29","02/01/2015 14:34" ));
-        assertEquals(false, TimeUtil.isNew("11/10/2015 06:29","11/10/2015 05:34" ));
-        assertEquals(false, TimeUtil.isNew("11/10/2015 06:29","02/11/2014 14:34" ));
+        assertThat(false).isEqualTo(TimeUtil.isNew("11/10/2015 06:29","02/01/2015 14:34" ));
+        assertThat(false).isEqualTo(TimeUtil.isNew("11/10/2015 06:29","11/10/2015 05:34" ));
+        assertThat(false).isEqualTo(TimeUtil.isNew("11/10/2015 06:29","02/11/2014 14:34" ));
     }
 }
