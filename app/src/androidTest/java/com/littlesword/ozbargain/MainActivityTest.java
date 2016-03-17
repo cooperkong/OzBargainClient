4ad98faca5a8569f1f31ebac3fb8a1a9d5edbf49
Wenchao Kong <wenchao.kong@gmail.com>
2016-03-17 14:39:52 +1100

    more espresso tests

--- app/src/androidTest/java/com/littlesword/ozbargain/MainActivityTest.java
+++ app/src/androidTest/java/com/littlesword/ozbargain/MainActivityTest.java
@@ -1,8 +1,11 @@
 package com.littlesword.ozbargain;
 
 import android.graphics.drawable.Drawable;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.intent.matcher.IntentMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.Gravity;
 
 import com.littlesword.ozbargain.categorylist.MainActivity;
 import com.littlesword.ozbargain.util.CommonUtil;
@@ -11,6 +14,28 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerActions.close;
+import static android.support.test.espresso.contrib.DrawerActions.open;
+import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
+import static android.support.test.espresso.contrib.DrawerMatchers.isOpen;
+import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
+import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.*;
 
 /**
@@ -22,6 +47,32 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
             MainActivity.class);
+
+    @Test
+    public void testDrawerLoaded(){
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+                .perform(open()); // Open Drawer
+
+        onView(allOf(withClassName(endsWith("AppCompatCheckedTextView")), withText("Home")))
+                .check(matches(withText("Home")))
+                .check(matches(isChecked()))
+                .check(matches(isDisplayed()))
+                .perform(click());//check Home category is displayed and selected
+
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed()));//drawer should be closed.
+
+    }
+
+    @Test
+    public void testSettingsMenu(){
+        openActionBarOverflowOrOptionsMenu(mActivityRule.getActivity());//open settings menu
+        onView(withText(R.string.action_settings))
+                .perform(click());// click on settings
+        onView(withText(R.string.pref_notification_title))
+                .check(matches(notNullValue()));//check if settings screen is opened
+    }
 
     @Test
     public void testCommonUtilgetTintedIcon(){
