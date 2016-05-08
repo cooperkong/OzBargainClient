29b6d3fab527d7a7de40ccf25e310c50308b2185
Wenchao Kong <wenchao.kong>
2016-05-08 18:57:11 +1000

    add unit test

--- app/src/test/java/com/littlesword/ozbargain/util/TimeUtilTest.java
+++ app/src/test/java/com/littlesword/ozbargain/util/TimeUtilTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.text.ParseException;
+import java.util.HashSet;
+
 import static com.google.common.truth.Truth.assertThat;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertFalse;
@@ -17,6 +19,8 @@ public class TimeUtilTest {
     @Rule
     public ExpectedException thrown= ExpectedException.none();
 
+    private HashSet<String> set = new HashSet<>();
+
     @Test
     public void testIsNew() throws ParseException {
         assertThat(true).isEqualTo(AppUtil.isNew("11/12/2015 14:29","11/12/2015 14:34" ));
@@ -26,6 +30,17 @@ public class TimeUtilTest {
         assertThat(false).isEqualTo(AppUtil.isNew("11/10/2015 06:29","02/01/2015 14:34" ));
         assertThat(false).isEqualTo(AppUtil.isNew("11/10/2015 06:29","11/10/2015 05:34" ));
         assertThat(false).isEqualTo(AppUtil.isNew("11/10/2015 06:29","02/11/2014 14:34" ));
+    }
+
+    @Test
+    public void testIsWanted(){
+        set.add("Gaming");
+        set.add("Electrical & Electronics");
+        set.add("Food & Grocery");
+        assertThat(true).isEqualTo(AppUtil.isWanted("Gaming",set ));
+        assertThat(false).isEqualTo(AppUtil.isWanted("Travel",set ));
+        assertThat(true).isEqualTo(AppUtil.isWanted("Food & Grocery",set ));
+
     }
 
 
